45c79ec5f64de1f2e09bddb2bb6f492f82338b77
Banday Chandini <140332965+BandayChandini@users.noreply.github.com>
2024-09-02 11:02:34 +0530

    commit

--- React/HandsOn/react-route-demo1/src/App.js
+++ React/HandsOn/react-route-demo1/src/App.js
@@ -6,20 +6,18 @@ import "./App.css";
 import Home from "./Pages/home";
 import Contact from "./Pages/contact";
 import Blogs from "./Pages/blogs";
-=======
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./Pages/home";
 import Blogs from "./Pages/blogs";
 import Contact from "./Pages/contact";
->>>>>>> 2cb043374f380c150701ef97bcbe22af3ff2a8ef
 import NoPage from "./Pages/nopage";
 import Layout from "./Pages/layout";
 function App() {
   return (
     <>
-<<<<<<< HEAD
+
     <BrowserRouter>
     <Routes>
     <Route path="/" element={<Layout/>}>
@@ -32,7 +30,7 @@ function App() {
       
     </Routes>
     </BrowserRouter>
-=======
+
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
@@ -43,7 +41,6 @@ function App() {
           <Route path="*" element={<NoPage />} />
         </Routes>
       </BrowserRouter>
->>>>>>> 2cb043374f380c150701ef97bcbe22af3ff2a8ef
     </>
   );
 }
